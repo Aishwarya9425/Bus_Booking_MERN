3edf857d132f59e071b128f41a6203a02a8ab8fa
Aishwarya <aishwarya9425@gmail.com>
2023-11-08 22:46:43 +0530

    update

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -1,13 +1,13 @@
-const mongoose = require("mongoose");
+import mongoose from 'mongoose';
 
-mongoose.connect(process.env.mongo_url);
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
 
-const db = mongoose.connection;
-
-db.on("connected", () => {
-  console.log("Mongo Db Connection Successfull");
-});
-
-db.on("error", () => {
-  console.log("Mongo Db Connection Failed");
-});
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,42 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-require("dotenv").config();
-const dbConfig = require("./config/dbConfig");
+const connectDB = require('./config/dbConfig');
+
+require('dotenv').config();
+
 const port = process.env.PORT || 5000;
+
+connectDB();
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const usersRoute = require("./routes/usersRoute");
-const busesRoute = require("./routes/busesRoute");
-const bookingsRoute = require("./routes/bookingsRoute");
+const usersRoute = require('./routes/usersRoute');
+const busesRoute = require('./routes/busesRoute');
+const bookingsRoute = require('./routes/bookingsRoute');
 
-app.use("/api/users", usersRoute);
-app.use("/api/buses", busesRoute);
-app.use("/api/bookings", bookingsRoute);
-const path = require("path");
-if(process.env.NODE_ENV === "production")
-{
-    app.use(express.static("client/build"));
-  
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
-    });
+app.use('/api/users', usersRoute);
+app.use('/api/buses', busesRoute);
+app.use('/api/bookings', bookingsRoute);
+const path = require('path');
+
+// for build.. 
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static('/var/data/uploads'));
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
 }
 
-app.listen(port, () => console.log(`Node server listening on port ${port}!`));
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
